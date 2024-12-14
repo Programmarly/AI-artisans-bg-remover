753d84a9d6106b3a427b19844228b6f67397bf1e
Harshit S Soni <harshit24337@gmail.com>
2024-12-14 16:07:33 +0530

    modification port again

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
+import os
 from backend import create_app
 from flask import render_template
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-PORT = os.getenv("PORT") or 5500 
 
 # Create Flask app instance
 app = create_app()
+
 @app.route('/')
 def home():
     return render_template('index.html')  # This serves the `index.html` file
 
 if __name__ == '__main__':
-    app.run(port= PORT,debug=True)
+    port = int(os.getenv("PORT", 5500))  # Default to 5500 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
