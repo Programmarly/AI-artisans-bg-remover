fdba363317d21d5a90643bc702dc17af96dc1ffa
Harshit S Soni <harshit24337@gmail.com>
2024-12-14 16:14:27 +0530

    modification port again

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def home():
     return render_template('index.html')  # This serves the `index.html` file
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5500))  # Default to 5500 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=True)
+   
+    app.run()
